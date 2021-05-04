363254edd8988f045aed47e1e2a5299840105708
Darrell Mayson <mcdoob@gmail.com>
2021-05-04 15:21:05 +1200

    refactor validation js

--- script.js
+++ script.js
@@ -28,16 +28,69 @@ function showError(input, message){
     formControl.className = 'form-control success';
   }
 
-
+  // check email validity
+  function checkEmailValid(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 
 
 // Event listeners
 form.addEventListener('submit', function(e){
   // disable default action
   e.preventDefault();
-  if(username.value === ''){
-    showError(username, 'Please enter a valid username.');
-  } else {
-    showSuccess(username);
-  }
+
+// Rough and ready but not optimal solution using if() statements.
+
+//   if(username.value === ''){
+//     showError(username, 'Please enter a valid username.');
+//   } else {
+//     showSuccess(username);
+//   }
+
+//   if(email.value === ''){
+//     showError(email, 'Please enter an email address.');
+//   } else if (!checkEmailValid(email.value)) {
+//     showError(email, 'Please enter a valid email address.')
+//   } else {
+//     showSuccess(email);
+//   }
+
+//   if(password.value === ''){
+//     showError(password, 'Please enter a valid password.');
+//   } else {
+//     showSuccess(password);
+//   }
+
+//   if(password2.value === ''){
+//     showError(password2, 'Please enter a valid password2.');
+//   } else {
+//     showSuccess(password2);
+//   }
+// });
+
+// END IF STATEMENTS
+
+// Better, optimised way to address validation.
+
+
+// Check required fields
+function checkRequired(inputArray){
+  inputArray.forEach(function(input){
+    if(input.value.trim() === ''){
+      showError(input, `${getFieldName(input)} is required.`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+
+// getFieldName: converst first letter of input ID to ucase then appends rest of input ID to this.  Clunky!!
+function getFieldName(input){
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+checkRequired([username, email, password, password2]);
+
 });
